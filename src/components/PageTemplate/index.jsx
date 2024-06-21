612aa84668fbf8dc018bc4e3451debd60617317a
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-06-21 12:05:21 -0300

    Implementação de children para template de página

---
+++ src/components/PageTemplate/index.jsx
@@ -0,0 +1,12 @@
+import { Header } from "../Header";
+import { Footer } from "../Footer";
+
+export function PageTemplate({ children }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
--- src/components/pages/HomePage/index.jsx
+++ src/components/pages/HomePage/index.jsx
@@ -1,21 +1,16 @@
-import { Header } from "../../Header";
 import { BannerSection } from "../../BannerSection";
 import { AboutMeSection } from "../../AboutMeSection";
 import { TechSection } from "../../TechSection";
 import { ProjectSection } from "../../ProjectSection";
-import { Footer } from "../../Footer";
+import { PageTemplate } from "../../PageTemplate";
 
 export function HomePage() {
   return (
-    <>
-      <Header />
-      <main>
-        <BannerSection />
-        <AboutMeSection />
-        <TechSection />
-        <ProjectSection />
-      </main>
-      <Footer />
-    </>
+    <PageTemplate>
+      <BannerSection />
+      <AboutMeSection />
+      <TechSection />
+      <ProjectSection />
+    </PageTemplate>
   );
 }
